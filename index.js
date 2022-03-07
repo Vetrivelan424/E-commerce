2242f04164dbeec7f9852a44b741ee9c1a4d8844
Vetrivelan <96474364+Vetrivelan424@users.noreply.github.com>
2022-03-07 14:23:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+const open=document.getElementById('open');
+const model=document.getElementById('model');
+const mod=document.getElementById('close');
+open.addEventListener('click',()=>{
+    model.classList.add('show');
+});
+
